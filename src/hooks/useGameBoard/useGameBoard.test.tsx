43e071ad5b3b28a41c4f27e731202289fa3e4c60
Alex Chandler <alexchandler@Alexs-MacBook-Pro.local>
2023-06-05 21:46:25 -0400

    fix tests for useGameBoard

--- src/hooks/useGameBoard/useGameBoard.test.tsx
+++ src/hooks/useGameBoard/useGameBoard.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react";
 import { Props, GameBoard, useGameBoard } from "./useGameBoard";
 
 describe("useGameBoard", () => {
@@ -18,26 +18,60 @@ describe("useGameBoard", () => {
     expect(gameBoard.totalShipCells).toBe(5);
   });
 
-  it("should update the game board and ship cells remaining when firing at a cell", () => {
+  it("should update the game board and ship cells remaining when firing at a ship cell", () => {
     const { result } = renderHook(() => useGameBoard(props));
 
     const gameBoard: GameBoard = result.current;
 
+    let row = -1;
+    let column = -1;
+
+    // Find the first ship cell
+    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+      for (let j = 0; j < gameBoard.gameBoard[i].length; j++) {
+        if (gameBoard.gameBoard[i][j].hasShip) {
+          row = i;
+          column = j;
+          return;
+        }
+      }
+    }
+
     // Fire at a cell with a ship
     act(() => {
-      gameBoard.fire(0, 0);
+      gameBoard.fire(row, column);
     });
 
-    expect(gameBoard.gameBoard[0][0].isHit).toBe(true);
+    expect(gameBoard.gameBoard[row][column].isHit).toBe(true);
     expect(gameBoard.shipCellsRemaining).toBe(4);
+  });
+
+  it("should update the game board correctly when firing at an empty cell", () => {
+    const { result } = renderHook(() => useGameBoard(props));
+
+    const gameBoard: GameBoard = result.current;
+
+    let row = -1;
+    let column = -1;
+
+    // Find the first empty cell
+    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+      for (let j = 0; j < gameBoard.gameBoard[i].length; j++) {
+        if (!gameBoard.gameBoard[i][j].hasShip) {
+          row = i;
+          column = j;
+          return;
+        }
+      }
+    }
 
     // Fire at a cell without a ship
     act(() => {
-      gameBoard.fire(1, 1);
+      gameBoard.fire(row, column);
     });
 
-    expect(gameBoard.gameBoard[1][1].isHit).toBe(true);
-    expect(gameBoard.shipCellsRemaining).toBe(4);
+    expect(gameBoard.gameBoard[row][column].isHit).toBe(true);
+    expect(gameBoard.shipCellsRemaining).toBe(5);
   });
 
   it("should reset the game board and ship cells remaining", () => {
@@ -45,12 +79,26 @@ describe("useGameBoard", () => {
 
     const gameBoard: GameBoard = result.current;
 
+    let row = -1;
+    let column = -1;
+
+    // Find the first ship cell
+    for (let i = 0; i < gameBoard.gameBoard.length; i++) {
+      for (let j = 0; j < gameBoard.gameBoard[i].length; j++) {
+        if (gameBoard.gameBoard[i][j].hasShip) {
+          row = i;
+          column = j;
+          return;
+        }
+      }
+    }
+
     // Fire at a cell to change the game board state
     act(() => {
-      gameBoard.fire(0, 0);
+      gameBoard.fire(row, column);
     });
 
-    expect(gameBoard.gameBoard[0][0].isHit).toBe(true);
+    expect(gameBoard.gameBoard[row][column].isHit).toBe(true);
     expect(gameBoard.shipCellsRemaining).toBe(4);
 
     // Reset the game board
@@ -58,7 +106,7 @@ describe("useGameBoard", () => {
       gameBoard.reset();
     });
 
-    expect(gameBoard.gameBoard[0][0].isHit).toBe(false);
+    expect(gameBoard.gameBoard[row][column].isHit).toBe(false);
     expect(gameBoard.shipCellsRemaining).toBe(5);
   });
 });
